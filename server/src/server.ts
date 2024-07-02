1a17b8d7827d7c9cc4384d6eb993a9a262c34447
RITAP28 <ritap2804manutd@gmail.com>
2024-07-02 21:50:55 +0530

    cors updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ const PORT = 7070;
 
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://side-hustle-frontend-alpha.vercel.app'],
         credentials: true,
     })
 );
