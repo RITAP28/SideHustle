5d4b6a88a1f4dfdf86591eadb29797159d5715cd
RITAP28 <ritap2804manutd@gmail.com>
2024-07-22 21:48:24 +0530

    routes modified

--- server/src/routes/upload.route.ts
+++ server/src/routes/upload.route.ts
@@ -18,25 +18,32 @@ export default (router: express.Router) => {
         maxCount: 1
         }
     ]), handleUploadVideo);
+    // normalRoutes
     router.get('/getallvideos', getAllVideos);
     router.get('/getuser', isAuthenticated, handleGetUser);
     router.get('/videos', isAuthenticated, handleGetOneVideo);
     router.get('/getCreator', isAuthenticated, handleGetCreatorInfo);
     router.post('/rolecreator', isAuthenticated, handleBecomeCreator);
+    // subscriptionRoutes
     router.post('/subscribe', isAuthenticated, handleSubscribe);
     router.get('/isSubscribed', isAuthenticated, handleIsSubscribed);
     router.get('/getAllSubscribers', isAuthenticated, handleGetAllSubscribers);
+    // friendRoutes
     router.get('/getallfriends', isAuthenticated, handleGetFriends);
     router.get('/getfriend', isAuthenticated, handleGetFriend);
+    // followRoutes
     router.post('/follow', isAuthenticated, handleFollow);
     router.post('/isFollowed', isAuthenticated, handleIsFollowed);
     router.get('/followCount', isAuthenticated, handleCountFollowers);
+    // reviewRoutes
     router.post('/submitReview', isAuthenticated, handleReviewVideo);
     router.get('/getAllReviews', isAuthenticated, handleGetAllReviewsByVideosId);
+    // starRoutes
     router.post('/starAVideo', isAuthenticated, handleStarVideo);
     router.get('/getAllStars', isAuthenticated, handleGetAllStarsByVideoId);
     router.get('/isStarred', isAuthenticated, handleIsStarred);
     router.delete('/removeStar', isAuthenticated, handleRemoveStar);
+    // dislikeRoutes
     router.get('/isDisliked', isAuthenticated, handleIsDisliked);
     router.post('/dislike', isAuthenticated, handleDislikeVideo);
     router.delete('/removeDislike', isAuthenticated, handleRemoveDislike);
